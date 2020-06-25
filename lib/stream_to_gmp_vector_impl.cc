8163ecfb1b3dac1e20cdd0b3ca45918ff5db035e
alekhgupta1441 <alekhgupta1441@gmail.com>
2020-06-25 18:10:40 +0530

    Updated stream_to_gmp_vector block

--- lib/stream_to_gmp_vector_impl.cc
+++ lib/stream_to_gmp_vector_impl.cc
@@ -58,19 +58,20 @@ stream_to_gmp_vector_impl::stream_to_gmp_vector_impl(const std::vector<int>& dpd
  */
 stream_to_gmp_vector_impl::~stream_to_gmp_vector_impl() {}
 void stream_to_gmp_vector_impl::gen_GMPvector(const gr_complex* const in,
-                                              int item,
-                                              int K_a,
-                                              int L_a,
-                                              int K_b,
-                                              int M_b,
-                                              int L_b,
-                                              cx_fcolvec& GMP_vector)
+                                       int item,
+                                       int K_a,
+                                       int L_a,
+                                       int K_b,
+                                       int M_b,
+                                       int L_b,
+                                       cx_fcolvec& GMP_vector)
 {
     /* Signal-and-Aligned Envelope */
     // stacking L_a elements in reverse order
     cx_fcolvec y_vec_arma1(L_a, fill::zeros);
-    for (int ii = L_a - 1; ii >= 0; ii--)
+    for (int ii = L_a - 1; ii >= 0; ii--) {
         y_vec_arma1(ii) = in[item - ii];
+    }
     GMP_vector.rows(0, L_a - 1) = y_vec_arma1;
 
     // store abs() of y_vec_arma1
@@ -79,12 +80,44 @@ void stream_to_gmp_vector_impl::gen_GMPvector(const gr_complex* const in,
 
     cx_fcolvec yy_temp;
     yy_temp = y_vec_arma1 % abs_y_vec_arma1;
-    GMP_vector.rows(L_a, 2 * L_a - 1) = yy_temp;
+    if (K_a > 1)
+        GMP_vector.rows(L_a, 2 * L_a - 1) = yy_temp;
     for (int kk = 2; kk < K_a; kk++) {
         // perform element-wise product using the overloaded % operator
         yy_temp = yy_temp % abs_y_vec_arma1;
 
         GMP_vector.rows(kk * L_a, (kk + 1) * L_a - 1) = yy_temp;
+    }
+
+    /* Signal-and-Delayed Envelope */
+    // stacking L_b+M_b elements in reverse order
+    cx_fcolvec y_vec_arma23(L_b + M_b, fill::zeros);
+    for (int ii = L_b + M_b - 1; ii >= 0; ii--)
+        y_vec_arma23(ii) = in[item - ii];
+
+    // L_b signal elements
+    cx_fcolvec y_vec_arma2 = y_vec_arma23.rows(0, L_b - 1);
+
+    // store abs() of y_vec_arma23
+    cx_fcolvec abs_y_vec_arma23(size(y_vec_arma23), fill::zeros);
+    abs_y_vec_arma23.set_real(abs(y_vec_arma23));
+
+    for (int mm = 1; mm < M_b + 1; mm++) {
+        // stacking L_b delayed signal-envelope elements
+        cx_fcolvec abs_y_vec_arma3 = abs_y_vec_arma23.rows(mm, L_b + mm - 1);
+
+        cx_fcolvec yy_temp;
+        yy_temp = y_vec_arma2 % abs_y_vec_arma3;
+        GMP_vector.rows(K_a * L_a + (mm - 1) * K_b * L_b,
+                        K_a * L_a + (mm - 1) * K_b * L_b + L_b - 1) = yy_temp;
+
+        for (int kk = 2; kk < K_b + 1; kk++) {
+            // perform element-wise product using the overloaded % operator
+            yy_temp = yy_temp % abs_y_vec_arma3;
+
+            GMP_vector.rows(K_a * L_a + (mm - 1) * K_b * L_b + (kk - 1) * L_b,
+                            K_a * L_a + (mm - 1) * K_b * L_b + kk * L_b - 1) = yy_temp;
+        }
     }
 }
 int stream_to_gmp_vector_impl::work(int noutput_items,
